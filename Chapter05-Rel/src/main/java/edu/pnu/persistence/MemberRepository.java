366194f28674a5d5d7bdbb632076b9aeaac92b2c
LeeYeongin <youngin5757@gmail.com>
2024-05-20 11:30:57 +0900

    연관관계 매핑

--- Chapter05-Rel/src/main/java/edu/pnu/persistence/MemberRepository.java
+++ Chapter05-Rel/src/main/java/edu/pnu/persistence/MemberRepository.java
@@ -1,9 +1,9 @@
 package edu.pnu.persistence;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import edu.pnu.domain.Member;
 
-public interface MemberRepository extends CrudRepository<Member, String> {
+public interface MemberRepository extends JpaRepository<Member, String> {
 
 }
